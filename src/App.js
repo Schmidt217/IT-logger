0fabe05ec9e081591f308b23eb583e2d123549b2
Mike Schmidt <schmidt217@gmail.com>
2021-11-11 12:00:59 -0700

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { useEffect } from "react";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
@@ -6,6 +6,8 @@ import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
 import AddTechModal from "./components/techs/AddTechModal";
 import TechListModal from "./components/techs/TechListModal";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
@@ -17,17 +19,19 @@ function App() {
 		M.AutoInit();
 	});
 	return (
-		<Fragment>
-			<SearchBar />
-			<div className="container">
-				<AddBtn />
-				<AddLogModal />
-				<EditLogModal />
-				<AddTechModal />
-				<TechListModal />
-				<Logs />
-			</div>
-		</Fragment>
+		<Provider store={store}>
+			<>
+				<SearchBar />
+				<div className="container">
+					<AddBtn />
+					<AddLogModal />
+					<EditLogModal />
+					<AddTechModal />
+					<TechListModal />
+					<Logs />
+				</div>
+			</>
+		</Provider>
 	);
 }
 
--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,16 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import TechSelectOptions from "../techs/TechSelectOptions";
+import PropTypes from "prop-types";
 import M from "materialize-css/dist/js/materialize.min.js";
+import { updateLog } from "../../actions/logActions";
 
-const EditLogModal = () => {
+const EditLogModal = ({ current, updateLog }) => {
 	const [message, setMessage] = useState([]);
 	const [attention, setAttention] = useState(false);
 	const [tech, setTech] = useState("");
+
+	useEffect(() => {
+		if (current) {
+			setMessage(current.message);
+			setAttention(current.attention);
+			setTech(current.tech);
+		}
+	}, [current]);
 
 	const onSubmit = () => {
 		if (message === "" || tech === "") {
 			M.toast({ html: "Please enter a message and tech" });
 		} else {
-			console.log(message, tech, attention);
+			const updatedLog = {
+				id: current.id,
+				message,
+				attention,
+				tech,
+				date: new Date(),
+			};
+
+			updateLog(updatedLog);
+			M.toast({ html: "Log updated" });
 
 			// Clear fields
 			setMessage("");
@@ -31,9 +52,6 @@ const EditLogModal = () => {
 							value={message}
 							onChange={(e) => setMessage(e.target.value)}
 						/>
-						<label htmlFor="message" className="active">
-							Log Message
-						</label>
 					</div>
 				</div>
 
@@ -48,9 +66,7 @@ const EditLogModal = () => {
 							<option value="" disabled>
 								Select Technicial
 							</option>
-							<option value="John Doe">John Doe</option>
-							<option value="Sam Smith">Sam Smith</option>
-							<option value="Sara Wilson">Sara Wilson</option>
+							<TechSelectOptions />
 						</select>
 					</div>
 				</div>
@@ -85,9 +101,18 @@ const EditLogModal = () => {
 	);
 };
 
+EditLogModal.propTypes = {
+	current: PropTypes.object,
+	updateLog: PropTypes.func.isRequired,
+};
+
 const modalStyle = {
 	width: "75%",
 	height: "75%",
 };
 
-export default EditLogModal;
+const mapStateToProps = (state) => ({
+	current: state.log.current,
+});
+
+export default connect(mapStateToProps, { updateLog })(EditLogModal);
